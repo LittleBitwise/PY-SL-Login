8a0310a2424ccdecf87889c513465f8702eb429f
LittleBitwise <92210138+LittleBitwise@users.noreply.github.com>
2024-09-09 12:29:10 +0300

    Update zerocode.py

--- zerocode.py
+++ zerocode.py
@@ -1,32 +1,25 @@
-# 0's in packet body are run-length encoded,
-# such that series of 1-255 zero bytes are encoded to take 2 bytes.
+# 0's are run-length encoded, such that
+# series of 1 to 255 zero-bytes are encoded into 2 bytes.
 # Example: '\x00\xff'
 #           ^^^^-- null-byte (zero)
 #               ^^^^-- count (255)
 
-def encode(input: bytes):
+def encode(input: bytes) -> bytes:
 	out = ''
-	in_zero = False
-	zeroes = 0
-	for byte in input:
-		match byte:
-			case 0x00 if in_zero == False:
-				in_zero = True
-				out += chr(0)
-				zeroes = 1
-				continue
+	i, n = 0, len(input)
+	while i < n: # While-loop for next-byte access.
+		match _ := input[i]:
 			case 0x00:
-				zeroes += 1
-			case b if in_zero == True:
-				in_zero = False
-				out += chr(zeroes)
-				out += chr(b)
-				zeroes = 0
+				# Look ahead from current index.
+				# Find next nonzero byte.
+				# Use index as length.
+				v = memoryview(input[i:]);
+				zeroes = next((x for x, byte in enumerate(v) if byte != 0x00), len(v))
+				out += '\0' + chr(zeroes)
+				i += zeroes
 			case b:
 				out += chr(b)
-	if zeroes != 0:
-		out += chr(zeroes)
-		zeroes = 0
+				i += 1
 	return bytes(out, encoding='ASCII')
 
 def decode(input: bytes) -> bytes:
@@ -47,13 +40,13 @@ def str2bin(s: str) -> str:
 	return ' '.join('{0:08b}'.format(c, 'b') for c in s)
 
 def debug():
-	# test = bytes.fromhex('B9 00 00 EF FF') # b'\xb9\x00\x00\xef\xff'
-	test = '0123 \x00\x00\x00\x00 456'.encode()
-	test = '\x00\x00\x00\x00'.encode()
-	print('original', test)
+	def test(s):
+		print('original', s)
+		print('encoded ', out := encode(s))
+		print('decoded ', out := decode(out))
+		print('OK:', s == out)
+		print('')
 
-	out = encode(test)
-	print('encoded ', out)
-
-	out = decode(out)
-	print('decoded ', out)
+	test('0123 \x00\x00\x00\x00 456'.encode())
+	test('\x00\x00\x00\x00'.encode())
+	test('\x00\x00\x00\x00\x01'.encode())
