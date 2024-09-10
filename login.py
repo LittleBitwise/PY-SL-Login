db546d512969ea8c2613e3bd6142e24ac4861062
LittleBitwise <92210138+LittleBitwise@users.noreply.github.com>
2024-09-10 14:48:20 +0300

    Update login.py

--- login.py
+++ login.py
@@ -32,27 +32,6 @@ def login(first: str, last: str, password: str):
 	proxy = xmlrpc.client.ServerProxy(uri)
 	return proxy.login_to_simulator(params)
 
-def udp_agent_update(seq):
-	data_header = struct.pack('>BLB', 0x00, seq, 0x00)
-	packed_data_message_ID = struct.pack('>B', 0x04)
-	packed_data_ID = agent_id_bytes + session_id_bytes
-	packed_data_QuatRots = zero_rot_bytes + zero_rot_bytes
-	packed_data_State = struct.pack('<B', 0x00)
-	packed_data_Camera = zero_vec_bytes + zero_vec_bytes + zero_vec_bytes + zero_vec_bytes
-	packed_data_Flags = struct.pack('<fLB', 0.0,0x00,0x00)
-
-	byte_sequence = [
-		packed_data_message_ID,
-		packed_data_ID,
-		packed_data_QuatRots,
-		packed_data_State,
-		packed_data_Camera,
-		packed_data_Flags,
-	]
-
-	packed_data = data_header + zerocode.encode(b''.join(byte_sequence)).encode()
-	udp.sendto(packed_data, (sim_ip, sim_port))
-
 result = login('first', 'last', 'password')
 
 log.info(result)
@@ -65,27 +44,70 @@ agent_id = result['agent_id']
 
 agent_id_bytes = uuid.UUID(agent_id).bytes
 session_id_bytes = uuid.UUID(session_id).bytes
+circuit_code_bytes = struct.pack('<L', circuit_code)
 zero_rot_bytes = struct.pack('<ffff', 0.0, 0.0, 0.0, 0.0)
 zero_vec_bytes = struct.pack('<fff', 0.0, 0.0, 0.0)
+zero_f_bytes = struct.pack('<f', 0.0)
 
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+def packet_header(message: int, sequence: int, flags=0, extra_byte=0, extra_header=None):
+	out = bytearray(struct.pack('>BLB', flags, sequence, extra_byte))
+	if extra_header is not None:
+		if extra_byte == len(header := bytes(extra_header)):
+			out.append(header)
+		else:
+			raise Exception('Extra byte does not match extra header size.')
+	out.extend(struct.pack('>L', message))
+	return bytes(out)
+
+def message(*args) -> bytes:
+	return b''.join(args)
+
 log.debug('UseCircuitCode')
-data = struct.pack('>BLBL', 0x00, 1, 0x00, 0xffff0003) + struct.pack('<L', circuit_code) + session_id_bytes + agent_id_bytes
-udp.sendto(data, (sim_ip, sim_port))
+udp.sendto(message(
+	packet_header(0xffff0003, 1),
+	circuit_code_bytes,
+	session_id_bytes,
+	agent_id_bytes,
+), (sim_ip, sim_port))
 
 log.debug('CompleteAgentMovement')
-data = struct.pack('>BLBL', 0x00, 2, 0x00, 0xffff00f9) + agent_id_bytes + session_id_bytes + struct.pack('<L', circuit_code)
-udp.sendto(data, (sim_ip, sim_port))
+udp.sendto(message(
+	packet_header(0xffff00f9, 2),
+	agent_id_bytes,
+	session_id_bytes,
+	circuit_code_bytes,
+), (sim_ip, sim_port))
 
 log.debug('AgentUpdate')
-udp_agent_update(3)
+udp.sendto(message(
+	packet_header(0x04, 3),
+	agent_id_bytes,
+	session_id_bytes,
+	zero_rot_bytes,
+	zero_rot_bytes,
+	bytes(0), # state
+	zero_vec_bytes,
+	zero_vec_bytes,
+	zero_vec_bytes,
+	zero_vec_bytes,
+	zero_f_bytes,
+	bytes(0), # inputs
+	bytes(0), # flags
+), (sim_ip, sim_port))
 
 log.debug('UUIDNameRequest')
-fix_ID = int('ffff0000', 16) + 235
-data_header = struct.pack('>BLB', 0x00, 4, 0x00)
-packed_data = agent_id_bytes
-packed_data += struct.pack('L', fix_ID) + struct.pack('>B',len(agent_id)) + packed_data
-udp.sendto(data, (sim_ip, sim_port))
+udp.sendto(message(
+	packet_header(0xffff00EB, 4),
+	agent_id_bytes,
+), (sim_ip, sim_port))
 
 log.debug('Finished!')
+
+# todo
+# INCOMING RegionHandshake
+# RegionHandshakeReply
+# AgentThrottle ?
+# AgentFOV ?
+# AgentHeightWidth ?
