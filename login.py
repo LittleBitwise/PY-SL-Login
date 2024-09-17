be0c6faba5fe9659519cb9149439f8be260d631e
LittleBitwise <92210138+LittleBitwise@users.noreply.github.com>
2024-09-17 21:38:13 +0300

    Update login.py

--- login.py
+++ login.py
@@ -1,5 +1,4 @@
 import logging
-import struct # UDP communication
 import zerocode, packet # local
 
 logging.basicConfig(level=logging.DEBUG, format='\t%(levelname)s\t%(message)s\n', filename='dump.log')
@@ -28,36 +27,39 @@ client.send(
 
 # Main connection loop.
 
-while data := client.recv():
+while data := client.receive():
 	(mID, mHZ) = packet.message_from_body(data[packet.BODY_BYTE:], packet.is_zerocoded(data))
 
-	if (mID, mHZ) not in [(6, 'Medium'), (13, 'Medium'), (15, 'Medium'), (189, 'Low')]:
+	if (mID, mHZ) not in [(12, 'High'), (15, 'High'), (20, 'High'), (6, 'Medium'), (13, 'Medium'), (15, 'Medium'), (189, 'Low')]:
 		log.debug(f'{packet.human_header(data)}\n\tUDP: {zerocode.byte2hex(data)}')
 
 	if (mID, mHZ) == (148, 'Low'):
 		log.debug('RegionHandshakeReply')
 		client.send(
-			packet.header(packet.RegionHandshakeReply, client.sequence),
+			packet.header(packet.RegionHandshakeReply, client.sequence, packet.ZEROCODED),
 			zerocode.encode_all(
 				client.agent_id_bytes,
-				client.session_id_bytes
+				client.session_id_bytes,
+				packet.u32.zero
 			),
 		)
 		log.debug('AgentUpdate')
 		client.send(
 			packet.header(packet.AgentUpdate, client.sequence, packet.ZEROCODED),
-			client.agent_id_bytes,
-			client.session_id_bytes,
-			packet.zero_rot_bytes, # BodyRotation
-			packet.zero_rot_bytes, # HeadRotation
-			packet.zero_1_bytes, # State
-			packet.zero_vec_bytes, # CameraCenter
-			packet.zero_vec_bytes, # CameraAtAxis
-			packet.zero_vec_bytes, # CameraLeftAxis
-			packet.zero_vec_bytes, # CameraUpAxis
-			packet.zero_f_bytes, # Far
-			packet.zero_4_bytes, # ControlFlags
-			packet.zero_1_bytes, # Flags
+			zerocode.encode_all(
+				client.agent_id_bytes,
+				client.session_id_bytes,
+				packet.f32.zero_rotation, # BodyRotation
+				packet.f32.zero_rotation, # HeadRotation
+				packet.u8.zero, # State
+				packet.f32.zero_vector, # CameraCenter
+				packet.f32.zero_vector, # CameraAtAxis
+				packet.f32.zero_vector, # CameraLeftAxis
+				packet.f32.zero_vector, # CameraUpAxis
+				packet.f32.zero, # Far
+				packet.u32.zero, # ControlFlags
+				packet.u8.zero, # Flags
+			)
 		)
 		pass
 
@@ -65,7 +67,9 @@ while data := client.recv():
 		pingID, _ = packet.unpack_sequence(data[7:12], 'b', '<i')
 		client.send(
 			packet.header(packet.CompletePingCheck, client.sequence),
-			bytes([pingID])
+			packet.pack_sequence(
+				packet.u8, pingID
+			)
 		)
 		continue
 
@@ -74,10 +78,8 @@ while data := client.recv():
 		client.send(
 			packet.header(packet.PacketAck, client.sequence),
 			packet.pack_sequence(
-				packet.u08, 1,
+				packet.u8, 1,
 				packet.u32, message_number,
 			)
 		)
 		continue
-
-log.debug('UDP connection closed, no data received.')
