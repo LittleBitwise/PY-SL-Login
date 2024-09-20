c51413c3f3d87dc0462be620a3d07ac523e29c2e
LittleBitwise <92210138+LittleBitwise@users.noreply.github.com>
2024-09-21 01:09:38 +0300

    Update login.py

--- login.py
+++ login.py
@@ -1,4 +1,4 @@
-import logging
+import logging, time
 import threading # for user input
 import zerocode, packet, template # local
 
@@ -17,10 +17,11 @@ ignored_logging = [
 	'PreloadSound',
 	'AttachedSound',
 	'ScriptControlChange',
+	'StartPingCheck',
 ]
 
 client = packet.client()
-log.info(client.login('firstname', 'lastname', 'password'))
+client.login('firstname', 'lastname', 'password')
 
 log.info('LOGGED IN')
 
@@ -35,6 +36,7 @@ def UserInputThread():
 def HandleUserInput():
 	global user_input
 	if user_input.lower() == 'q':
+		SendLogoutRequest()
 		exit()
 	SendChatFromViewer(user_input)
 	user_input = None
@@ -96,9 +98,17 @@ def SendPacketAck(message_number: int):
 			packet.u32, message_number,
 		)
 	)
+def SendLogoutRequest():
+	client.send(
+		packet.header(template.message['LogoutRequest'], client.sequence),
+		packet.pack_sequence(
+			packet.uuid, client.agent_id_bytes,
+			packet.uuid, client.session_id_bytes,
+		)
+	)
 def SendChatFromViewer(text: str):
 	log.info(f'Sending chat: {text}')
-	text_bytes = text.encode('utf-8')
+	text_bytes = text.encode('utf-8') + b'\x00'
 	client.send(
 		packet.header(template.message['ChatFromViewer'], client.sequence, packet.ZEROCODED),
 		zerocode.encode(
@@ -106,20 +116,52 @@ def SendChatFromViewer(text: str):
 				packet.uuid, client.agent_id_bytes,
 				packet.uuid, client.session_id_bytes,
 				packet.variable2, len(text_bytes),
-				packet.string, text_bytes,
-				packet.u8, 1,
-				packet.u32, 0
+				packet.string, text_bytes,	# {	Message			Variable 2	}
+				packet.u8, 1,				# {	Type			U8			}
+				packet.u32, 0,				# {	Channel			S32			}
 			)
 		)
 	)
+def HandleChatFromSimulator(data: bytes):
+	data = packet.unpack_sequence(
+		data[10:],
+		packet.variable1,
+		packet.string,	# {	FromName		Variable 1	}
+		packet.uuid,	# {	SourceID		LLUUID		}
+		packet.uuid,	# {	OwnerID			LLUUID		}
+		packet.u8,		# {	SourceType		U8			}
+		packet.u8,		# {	ChatType		U8			}
+		packet.u8,		# {	Audible			U8			}
+		packet.vector,	# {	Position		LLVector3	}
+		packet.variable2,
+		packet.string,	# {	Message			Variable 2	}
+	)
+	from_name = data[1]
+	src_type  = data[4]
+	chat_type = data[5]
+	audible   = data[6]
+	message   = data[9]
+	log.info(f'{src_type} {chat_type} {audible} | {from_name}: {message}')
+	if chat_type < 4: # whisper, say, shout
+		from_name = str(from_name, 'utf-8').strip()
+		message = str(data[9], 'utf-8').strip()
+		print(f'{from_name}: {message}')
 def HandleKickUser(data: bytes):
 	data = packet.unpack_sequence(
 		data[48:],
-		packet.u16.format,
+		packet.variable2.format,
 		packet.string.format
 	)
 	reason = packet.string.from_bytes(data[-1])
 	log.warning(f'Disconnected: {reason}')
+
+def TimePassed(seconds: float) -> bool:
+	present = time.time()
+	earlier = getattr(TimePassed, "time", present)
+	elapsed = (present - earlier) >= seconds
+	TimePassed.time = present if elapsed else earlier
+	return elapsed
+
 
 # Login preamble.
 
@@ -145,6 +187,12 @@ while data := client.receive():
 
 	if message == 'RegionHandshake':
 		SendRegionHandshakeReply()
+		SendAgentUpdate()
+
+	if message == 'ChatFromSimulator':
+		HandleChatFromSimulator(data)
+
+	if TimePassed(0.5):
 		SendAgentUpdate()
 
 	if message == 'KickUser':
