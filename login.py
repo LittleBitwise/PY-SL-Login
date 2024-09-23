05e5ff7813d44cf1b2a831fba3847b8d6f8d94eb
LittleBitwise <92210138+LittleBitwise@users.noreply.github.com>
2024-09-23 09:59:57 +0300

    Update login.py
    THIS COMMIT IS AN ABSOLUTE MESS, USE THE PREVIOUS ONE

--- login.py
+++ login.py
@@ -1,6 +1,6 @@
 import logging, time
 import threading # for user input
-import zerocode, packet, template # local
+import zerocode, packet, template, im as chat_util # local
 
 logging.basicConfig(level=logging.DEBUG, format='\t%(levelname)s\t%(message)s\n', filename='dump.log')
 log = logging.getLogger()
@@ -18,6 +18,7 @@ ignored_logging = [
 	'AttachedSound',
 	'ScriptControlChange',
 	'StartPingCheck',
+	'ViewerEffect',
 ]
 
 client = packet.client()
@@ -35,10 +36,22 @@ def UserInputThread():
 		user_input = input()
 def HandleUserInput():
 	global user_input
+	AGENT_CONTROL_TURN_LEFT = 0x02000000
+	AGENT_CONTROL_TURN_RIGHT = 0x04000000
 	if user_input.lower() == 'q':
 		SendLogoutRequest()
 		exit()
-	SendChatFromViewer(user_input)
+	if user_input == 'A':
+		log.info(f'sending input: {user_input}')
+		SendAgentUpdate(AGENT_CONTROL_TURN_LEFT)
+	elif user_input == 'D':
+		log.info(f'sending input: {user_input}')
+		SendAgentUpdate(AGENT_CONTROL_TURN_RIGHT)
+	elif user_input == 'S':
+		log.info(f'sending input: {user_input}')
+		SendAgentUpdate(0)
+	else:
+		SendImprovedInstantMessage(user_input)
 	user_input = None
 	pass
 
@@ -74,78 +87,105 @@ def SendRegionHandshakeReply():
 			packet.u32.zero
 		),
 	)
-def SendAgentUpdate():
+def SendAgentUpdate(control: int=0):
 	client.send(
 		packet.header(template.message['AgentUpdate'], client.sequence, packet.ZEROCODED),
 		zerocode.encode_all(
-			client.agent_id_bytes,
-			client.session_id_bytes,
-			b'\x00' * 106,
+			# client.agent_id_bytes,
+			# client.session_id_bytes,
+			# b'\x00' * 106,
+			packet.pack_sequence(
+				packet.uuid,		client.agent_id_bytes,
+				packet.uuid,		client.session_id_bytes,
+				packet.rotation,	packet.rotation.zero,	# BodyRotation		16
+				packet.rotation,	packet.rotation.zero,	# HeadRotation		16
+				packet.u8,			0,						# State				1
+				packet.vector,		(128.0, 128.0, 30.0),	# CameraCenter		12
+				packet.vector,		(0.0, 1.0, 0.0),		# CameraAtAxis		12
+				packet.vector,		(1.0, 0.0, 0.0),		# CameraLeftAxis	12
+				packet.vector,		(0.0, 0.0, 1.0),		# CameraUpAxis		12
+				packet.f32,			16.0,					# Far				4
+				packet.u32,			control if control else 0,				# ControlFlags		4
+				packet.u8,			0,						# Flags				1
+			)
 		)
 	)
 def SendCompletePingCheck(pingID: int):
 	client.send(
 		packet.header(template.message['CompletePingCheck'], client.sequence),
 		packet.pack_sequence(
-			packet.u8, pingID
+			packet.u8,	pingID
 		)
 	)
 def SendPacketAck(message_number: int):
 	client.send(
 		packet.header(template.message['PacketAck'], client.sequence),
 		packet.pack_sequence(
-			packet.u8, 1,
-			packet.u32, message_number,
+			packet.u8,	1,
+			packet.u32,	message_number,
 		)
 	)
 def SendLogoutRequest():
 	client.send(
 		packet.header(template.message['LogoutRequest'], client.sequence),
+		client.agent_id_bytes,
+		client.session_id_bytes,
+	)
+def SendAgentHeightWidth():
+	client.send(
+		packet.header(template.message['AgentHeightWidth'], client.sequence),
+		client.agent_id_bytes,
+		client.session_id_bytes,
+		client.circuit_code_bytes,
 		packet.pack_sequence(
-			packet.uuid, client.agent_id_bytes,
-			packet.uuid, client.session_id_bytes,
+			packet.u32,		0,
+			packet.u16,		800,
+			packet.u16,		600,
+		)
+	)
+def SendAgentFOV():
+	client.send(
+		packet.header(template.message['AgentFOV'], client.sequence),
+		client.agent_id_bytes,
+		client.session_id_bytes,
+		client.circuit_code_bytes,
+		packet.pack_sequence(
+			packet.u32,		0,
+			packet.f32,		6.233185307179586
 		)
 	)
 def SendChatFromViewer(text: str):
 	log.info(f'Sending chat: {text}')
-	text_bytes = text.encode('utf-8') + b'\x00'
 	client.send(
-		packet.header(template.message['ChatFromViewer'], client.sequence, packet.ZEROCODED),
-		zerocode.encode(
-			packet.pack_sequence(
-				packet.uuid, client.agent_id_bytes,
-				packet.uuid, client.session_id_bytes,
-				packet.variable2, len(text_bytes),
-				packet.string, text_bytes,	# {	Message			Variable 2	}
-				packet.u8, 1,				# {	Type			U8			}
-				packet.u32, 0,				# {	Channel			S32			}
+		packet.header(template.message['ChatFromViewer'], client.sequence),
+		client.agent_id_bytes,
+		client.session_id_bytes,
+		chat_util.build_chat(text)
+	)
+def HandleChatFromSimulator(data: bytes):
+	chat = chat_util.parse_chat(data)
+	log.info(f'{chat.SourceType} {chat.Type} {chat.Audible} | {chat.FromName}: {chat.Message}')
+	if chat.Type <= chat_util.ChatType.Say:
+		print(f'{chat.FromName}: {chat.Message}')
+def HandleImprovedInstantMessage(data: bytes):
+	im = chat_util.parse_im(data)
+	log.info(im)
+	if im.Dialog == chat_util.Dialog.IM:
+		print(f'IM - {im.FromAgentName}: {im.Message}')
+def SendImprovedInstantMessage(text: str):
+	log.info(f'Sending IM: {text}')
+	client.send(
+		packet.header(template.message['ImprovedInstantMessage'], client.sequence, packet.ZEROCODED|packet.RELIABLE),
+		zerocode.encode_all(
+			chat_util.build_im(
+				text,
+				'llScriptProfiler Resident',
+				client.agent_id_bytes,
+				client.session_id_bytes,
+				packet.uuid.from_string('779e1d56-5500-4e22-940a-cd7b5adddbe0')
 			)
 		)
 	)
-def HandleChatFromSimulator(data: bytes):
-	data = packet.unpack_sequence(
-		data[10:],
-		packet.variable1,
-		packet.string,	# {	FromName		Variable 1	}
-		packet.uuid,	# {	SourceID		LLUUID		}
-		packet.uuid,	# {	OwnerID			LLUUID		}
-		packet.u8,		# {	SourceType		U8			}
-		packet.u8,		# {	ChatType		U8			}
-		packet.u8,		# {	Audible			U8			}
-		packet.vector,	# {	Position		LLVector3	}
-		packet.variable2,
-		packet.string,	# {	Message			Variable 2	}
-	)
-	from_name = data[1]
-	src_type  = data[4]
-	chat_type = data[5]
-	audible   = data[6]
-	message   = data[9]
-	log.info(f'{src_type} {chat_type} {audible} | {from_name}: {message}')
-	if chat_type < 4: # whisper, say, shout
-		from_name = str(from_name, 'utf-8').strip()
-		message = str(data[9], 'utf-8').strip()
-		print(f'{from_name}: {message}')
 def HandleKickUser(data: bytes):
 	data = packet.unpack_sequence(
 		data[48:],
@@ -188,12 +228,17 @@ while data := client.receive():
 	if message == 'RegionHandshake':
 		SendRegionHandshakeReply()
 		SendAgentUpdate()
+		# SendAgentHeightWidth()
+		# SendAgentFOV()
 
 	if message == 'ChatFromSimulator':
 		HandleChatFromSimulator(data)
 
-	if TimePassed(0.5):
-		SendAgentUpdate()
+	if message == 'ImprovedInstantMessage':
+		HandleImprovedInstantMessage(data)
+
+	# if TimePassed(0.5):
+	# 	SendAgentUpdate()
 
 	if message == 'KickUser':
 		HandleKickUser(data)
